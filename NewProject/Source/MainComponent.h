4ae27b84225d1694e143639ba98d041d0e458c16
Jazzstang <44452732+Jazzstang@users.noreply.github.com>
2025-06-05 22:29:22 +0200

    Update MainComponent.h

--- NewProject/Source/MainComponent.h
+++ NewProject/Source/MainComponent.h
@@ -3,19 +3,25 @@
 #include <JuceHeader.h>
 #include "MidiTrackComponent.h"
 
+//==============================================================================
 class MainComponent : public juce::Component
 {
 public:
     MainComponent();
+    ~MainComponent() override;
+
+    void paint(juce::Graphics&) override;
     void resized() override;
 
 private:
-    juce::OwnedArray<MidiTrackComponent> tracks;
-    juce::TextButton addTrackButton { "+" };
+    juce::TextButton addTrackButton { "Ajouter une piste" };
+
+    // Important : trackContainer doit être détruit **après** les composants qui l’utilisent (comme trackViewport)
+    std::unique_ptr<juce::Component> trackContainer;
+
     juce::Viewport trackViewport;
-    juce::Component trackContainer;
 
-    void addNewTrack();
+    juce::OwnedArray<MidiTrackComponent> midiTracks;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(MainComponent)
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
